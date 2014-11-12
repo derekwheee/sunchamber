05832f9358fff2cd360cae18849014308bfaeaef
Derek Wheelden <derek.wheelden@gmail.com>
2014-11-12 09:31:46 -0500

    Updating sunchamber to work with nodebot

--- server.js
+++ server.js
@@ -2,12 +2,7 @@ var express = require('express');
     app     = express(),
     server  = require('http').Server(app),
     io      = require('socket.io')(server),
-    port    = Number(process.env.PORT || 5000),
-
-    status = {
-        in1 : 1,
-        in2 : 0
-    };
+    port    = Number(process.env.PORT || 5000);
 
 server.listen(port, function() {
     console.log("Listening on " + port);
@@ -17,17 +12,8 @@ app.use(express.static(__dirname + '/'));
 
 io.on('connection', function (socket) {
 
-    socket.emit('in1', status.in1);
-    socket.emit('in2', status.in2);
-
-    socket.on('in1', function (data) {
-        status.in1 = data;
-        io.emit('in1', status.in1);
-    });
-
-    socket.on('in2', function (data) {
-        status.in1 = data;
-        io.emit('in2', status.in1);
+    socket.on('control', function (data) {
+        io.emit('move', data);
     });
 
 });
